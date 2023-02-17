9244d3d971c5bde84e83fb91fc804e40bce49f60
Buddy Williams <bwilliams@nevo.com>
2023-02-17 09:42:25 -0500

    wip: establish sql server connector

--- mssql_init.sql
+++ mssql_init.sql
@@ -21,22 +21,34 @@ GO
 
 IF OBJECT_ID('Products', 'U') IS NULL
 BEGIN
-  CREATE TABLE Products (ID int, ProductName nvarchar(max));
+  CREATE TABLE Products (ID int IDENTITY(1,1), ProductName nvarchar(max));
 END;
 GO
+-- DROP TABLE Products;
+
+EXEC sys.sp_cdc_enable_table
+@source_schema = N'dbo',
+@source_name   = N'Products', 
+@role_name     = NULL,  
+@filegroup_name = NULL,
+@supports_net_changes = 0
+GO
+
+-- EXEC sys.sp_cdc_help_change_data_capture
+-- GO
 
 IF NOT EXISTS (SELECT * FROM Products WHERE ID = 1)
 BEGIN
-    INSERT INTO Products (ID, ProductName)
-         VALUES (1, 'Milk'),
-                (2, 'Butter'),
-                (3, 'Broccoli'),
-                (4, 'Chicken'),
-                (5, 'Soy Sauce'),
-                (6, 'Red Bell Pepper'),
-                (7, 'Green Onions'),
-                (8, 'Olive Oil'),
-                (9, 'Eggs');
+    INSERT INTO Products (ProductName)
+         VALUES ('Milk'),
+                ('Butter'),
+                ('Broccoli'),
+                ('Chicken'),
+                ('Soy Sauce'),
+                ('Red Bell Pepper'),
+                ('Green Onions'),
+                ('Olive Oil'),
+                ('Eggs');
 END
 
 SELECT * from Products;
